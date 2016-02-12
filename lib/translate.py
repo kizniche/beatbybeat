330b5e5545a4b1c4bbd2712146e465d4fbda4e8c
Kizniche <KyleTGabriel@gmail.com>
2016-02-12 01:10:31 -0500

    Fix averaging calculations

--- lib/translate.py
+++ lib/translate.py
@@ -8,30 +8,30 @@ import RPi.GPIO as GPIO
 import time
 
 def morse_translator(verbose, gpio, period, max_bpm):
-    print '\nA tempo needs to be set to determine the duration of dashes and dots and between new words and letters.', \
+    print '\nA tempo needs to be set to determine the duration of dashes and dots and between words and letters.', \
           '\nBegin tapping a tempo to set the duration between letters.', \
           '\nTiming will begin upon the first buttom press and be measured for {} milliseconds.\n'.format(period)
     
     milliseconds_space_dash = detect_bpm(verbose, gpio, period, max_bpm)
     milliseconds_dot = milliseconds_space_dash / 3
     milliseconds_between_words = milliseconds_dot * 7
-    print '\nTempo acquired (ms = milliseconds, BPM = beats per minute):'
-    print 'Between words: {} ms ({} BPM)'.format(milliseconds_between_words, 60000 / milliseconds_between_words)
-    print 'Between letters: {} ms ({} BPM)'.format(milliseconds_space_dash, 60000 / milliseconds_space_dash)
-    print 'Dash: {} ms ({} BPM)'.format(milliseconds_space_dash, 60000 / milliseconds_space_dash)
-    print 'Dot: {} ms ({} BPM)'.format(milliseconds_dot, 60000 / milliseconds_dot)
+    print '\nDurations acquired (ms = milliseconds):'
+    print 'Between words: {} ms'.format(milliseconds_between_words)
+    print 'Between letters: {} ms'.format(milliseconds_space_dash)
+    print 'Dash: {} ms'.format(milliseconds_space_dash)
+    print 'Dot: {} ms'.format(milliseconds_dot)
     print '\nWait 5 seconds before beginning Morse code...\n'
     time.sleep(5)
     
     print 'Ready! Morse code translation will begin automatically.'
-    print 'A new letter will begin when the duration between taps is greater than or equal to {} ms ({} BPM).'.format(milliseconds_space_dash, 60000 / milliseconds_space_dash)
-    print 'A new word will begin when the duration between taps is greater than or equal to {} ms ({} BPM).\n'.format(milliseconds_between_words, 60000 / milliseconds_between_words)
+    print 'A new letter will begin when the duration between dashes or dots is greater than or equal to {} ms.'.format(milliseconds_space_dash)
+    print 'A new word will begin when the duration between dashes or dots is greater than or equal to {} ms.\n'.format(milliseconds_between_words)
 
 
 def detect_bpm(verbose, gpio, period, max_bpm):
     sleep_time = 1 / max_bpm  # Calculate minimum sleep duration possible to detect the maximum BPM
     beat_count_period = 1
-    duration_average = 0
+    duration_total = 0
     detected_milliseconds = 0
     first_beat = True
 
@@ -50,12 +50,12 @@ def detect_bpm(verbose, gpio, period, max_bpm):
                 time_now = int(round(time.time()*1000))
 
                 if first_beat:
-                    duration_average = int(round(time.time()*1000)) - time_between_beatcount
+                    duration_total = int(round(time.time()*1000)) - time_between_beatcount
                     first_beat = False
                 else:
-                    duration_average = ((int(round(time.time()*1000)) - time_between_beatcount) + duration_average) / 2
+                    duration_total = (int(round(time.time()*1000)) - time_between_beatcount) + duration_total
                 if verbose:
-                    print 'Beat number {}, time now: {}, time diff: {}, Average: {}'.format(beat_count_period, time_now, time_now - time_between_beatcount, duration_average)
+                    print 'Beat number {}, Time: {}, Diff: {} ms, Average: {} ms'.format(beat_count_period, time_now, time_now - time_between_beatcount, duration_total / (beat_count_period - 1))
 
                 time_between_beatcount = time_now
                 beat_count_period += 1
@@ -63,7 +63,7 @@ def detect_bpm(verbose, gpio, period, max_bpm):
                     time.sleep(sleep_time)
 
         if int(round(time.time()*1000)) - start_time > period:
-            detected_milliseconds = duration_average
+            detected_milliseconds = duration_total / (beat_count_period - 2)
         time.sleep(sleep_time)
 
     return detected_milliseconds
